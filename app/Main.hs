7848a4551cadb46390042a872f3095840ef28525
Jonathon Yallop <jyallop21@gmail.com>
2018-11-23 15:11:03 -0700

    inital commit

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Lib
+import Euterpea
+
+main :: IO ()
+main = 
+    someFunc
+
+t251 :: Music Pitch
+t251 = let  dMinor = d 4 wn :=: a 4 wn :=: a 4 wn
+            gMajor = g 4 wn :=: d 5 wn :=: d 5 wn
+            cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+        in dMinor :+: gMajor :+: cMajor
+
+twoFiveOne :: Pitch -> Dur -> Music Pitch
+twoFiveOne = undefined
